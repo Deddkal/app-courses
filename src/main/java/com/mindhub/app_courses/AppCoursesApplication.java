2075320a01556c209463094a2713fb37254e6863
Deddkal <m.araolaza89@gmail.com>
2024-09-18 15:42:52 -0300

    first commit

---
+++ src/main/java/com/mindhub/app_courses/AppCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.app_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCoursesApplication.class, args);
+	}
+
+}
